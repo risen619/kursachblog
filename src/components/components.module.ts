bc57eda95766123a08fe0749744cb5ef2c783513
Andrew Komar <risen619@gmail.com>
2018-09-23 21:30:08 +0300

    front end

---
+++ src/components/components.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+	declarations: [
+		MainComponent
+	],
+	imports: [],
+	providers: [],
+	exports: [
+		MainComponent
+	]
+})
+export class ComponentsModule { }
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { ComponentsModule } from './components.module';
+import { MainComponent } from './main/main.component';
+
+export {
+    ComponentsModule,
+    MainComponent
+};
